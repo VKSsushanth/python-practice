d4c041528bbcce20640993f4409beb01a20f92b5
sushanth vattikuti <93367777+xxDEFIBRILATORxx@users.noreply.github.com>
2022-03-14 13:41:20 +0000

    hi

---
+++ squareRoot.py
@@ -0,0 +1,5 @@
+import math
+a = float(input("enter the value of a : "))
+d = math.sqrt (a)
+print("square root is : ",d)
+# import math import  is used to find the sqrt of a value with ease. It is used to find powers of a number too. In general this function adds more math functions.
