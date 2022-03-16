feb415f939742448386670ce8eff580aecff3cd9
sushanth vattikuti <93367777+xxDEFIBRILATORxx@users.noreply.github.com>
2022-03-16 16:48:36 +0000

    .

---
+++ KMtoMILES.py
@@ -0,0 +1,5 @@
+KM = float(input("enter the vakue in km : "))
+
+MILES = float((KM*0.621371))
+
+print("the value in miles is : ",MILES)
---
+++ farenhiet.py
@@ -0,0 +1,3 @@
+c = float(input("enter teh tremperatue in celcius : "))
+f = (9/5)*c + 32
+print("the value in farenhiet is : ",f)
