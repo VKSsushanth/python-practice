ea926ddb77d2c4569f4598c7db3cbcf91b5ef56f
sushanth vattikuti <93367777+xxDEFIBRILATORxx@users.noreply.github.com>
2022-03-17 15:44:40 +0000

    .

---
+++ Max.py
@@ -0,0 +1,6 @@
+a = float(input("enter a number of your choice : "))
+b = float(input("enter a number of your choice : "))
+c = float(input("enter a number of your choice : "))
+
+print(max(a,b,c))
+#the function max(x,y,z) is used to find the maximum number also we can use if else statements to do this question but we prefer this method to simplify this program.
